21845aa6b982562fadd626a87055470a2056e91f
Stefan Eder <darkmaster.eder@gmail.com>
2022-10-19 00:30:40 +0200

    write test for BookRoom function in de BookingRequestProcesser class

--- RoomBookingApp.Core.Test/RoomBookingRequestProcessorTest.cs
+++ RoomBookingApp.Core.Test/RoomBookingRequestProcessorTest.cs
@@ -1,10 +1,36 @@
 ﻿using System;
+
 namespace RoomBookingApp.Core
 {
     public class RoomBookingRequestProcessorTest
     {
-        public RoomBookingRequestProcessorTest()
+        [Fact]
+        public void ShouldReturnRoomBookingResponseWithRequestValues ()
         {
+            // Arrange
+            var request = new RoomBookingRequest
+            {
+                FullName = "Max Mustermann",
+                Email = "test@test.com",
+                Date = new DateTime(2022, 10, 18)
+            };
+
+            var processor = new RoomBookingRequestProcessor();
+
+            // Act
+            RoomBookingResult result = processor.BookRoom(request);
+
+            // Assert
+            // Assert.NotNull (result); // Standard Assert
+            // Assert.Equal(request.FullName, result.FullName);
+            // Assert.Equal(request.Email, result.Email);
+            // Assert.Equal(request.Date, result.Date);
+
+            result.ShouldNotBeNull(); // Assert mit Shouldly
+            result.FullName.ShouldBe(request.FullName);
+            result.Email.ShouldBe(request.Email);
+            result.Date.ShouldBe(request.Date);
+
         }
     }
 }
