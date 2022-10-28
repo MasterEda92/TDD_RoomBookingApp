363b09d65dedbd469588d6e490fe1b76e5a50e59
Stefan Eder <darkmaster.eder@gmail.com>
2022-10-28 14:43:36 +0200

    In the RoomBookingRequestProcessor save a RoomBooking object via the IRoomBookilngService

--- RoomBookingApp.Core.Test/RoomBookingRequestProcessorTest.cs
+++ RoomBookingApp.Core.Test/RoomBookingRequestProcessorTest.cs
@@ -1,30 +1,41 @@
-﻿using RoomBookingApp.Core.Models;
+﻿using RoomBookingApp.Core.DataServices;
+using RoomBookingApp.Core.Models;
 using RoomBookingApp.Core.Processors;
+using Moq;
+using RoomBookingApp.Core.Domain;
 
 namespace RoomBookingApp.Core
 {
     public class RoomBookingRequestProcessorTest
     {
         private RoomBookingRequestProcessor _processor;
+        private RoomBookingRequest _request;
+        private Mock<IRoomBookingService> _roomBookingServiceMock;
 
         public RoomBookingRequestProcessorTest()
         {
-            _processor = new RoomBookingRequestProcessor();
-        }
-
-        [Fact]
-        public void ShouldReturnRoomBookingResponseWithRequestValues ()
-        {
             // Arrange
-            var request = new RoomBookingRequest
+            
+            _request = new RoomBookingRequest
             {
                 FullName = "Max Mustermann",
                 Email = "test@test.com",
                 Date = new DateTime(2022, 10, 18)
             };
 
+            _roomBookingServiceMock = new Mock<IRoomBookingService>();
+            _processor = new RoomBookingRequestProcessor(_roomBookingServiceMock.Object);
+
+        }
+
+        [Fact]
+        public void ShouldReturnRoomBookingResponseWithRequestValues ()
+        {
+            // Arrange
+            
+
             // Act
-            RoomBookingResult result = _processor.BookRoom(request);
+            RoomBookingResult result = _processor.BookRoom(_request);
 
             // Assert
 
@@ -36,9 +47,9 @@ namespace RoomBookingApp.Core
 
             // Shouldly
             result.ShouldNotBeNull();
-            result.FullName.ShouldBe(request.FullName);
-            result.Email.ShouldBe(request.Email);
-            result.Date.ShouldBe(request.Date);
+            result.FullName.ShouldBe(_request.FullName);
+            result.Email.ShouldBe(_request.Email);
+            result.Date.ShouldBe(_request.Date);
 
         }
 
@@ -48,6 +59,31 @@ namespace RoomBookingApp.Core
             var exception = Should.Throw<ArgumentNullException>(() => _processor.BookRoom(null));
 
             exception.ParamName.ShouldBe("bookingRequest");
+        }
+
+        [Fact]
+        public void Should_Save_Room_Booking_Request()
+        {
+            // Arrange
+            RoomBooking savedBooking = null;
+            _roomBookingServiceMock.Setup(q => q.Save(It.IsAny<RoomBooking>()))
+                .Callback<RoomBooking>(booking =>
+                {
+                    savedBooking = booking;
+                });
+
+            // Act
+            _processor.BookRoom(_request);
+
+            // Assert
+
+            // Verify the Save Method was called once
+            _roomBookingServiceMock.Verify(q => q.Save(It.IsAny<RoomBooking>()), Times.Once);
+
+            savedBooking.ShouldNotBeNull();
+            savedBooking.FullName.ShouldBe(_request.FullName);
+            savedBooking.Email.ShouldBe(_request.Email);
+            savedBooking.Date.ShouldBe(_request.Date);
         }
     }
 }
--- RoomBookingApp.Core/Processors/RoomBookingRequestProcessor.cs
+++ RoomBookingApp.Core/Processors/RoomBookingRequestProcessor.cs
@@ -1,11 +1,16 @@
-﻿using RoomBookingApp.Core.Models;
+﻿using RoomBookingApp.Core.DataServices;
+using RoomBookingApp.Core.Domain;
+using RoomBookingApp.Core.Models;
 
 namespace RoomBookingApp.Core.Processors
 {
     public class RoomBookingRequestProcessor
     {
-        public RoomBookingRequestProcessor()
+        private readonly IRoomBookingService _service;
+
+        public RoomBookingRequestProcessor(IRoomBookingService service)
         {
+            this._service = service;
         }
 
         public RoomBookingResult BookRoom(RoomBookingRequest ?bookingRequest)
@@ -14,6 +19,13 @@ namespace RoomBookingApp.Core.Processors
             {
                 throw new ArgumentNullException(nameof(bookingRequest));
             }
+
+            _service.Save(new RoomBooking
+            {
+                FullName = bookingRequest.FullName,
+                Email = bookingRequest.Email,
+                Date = bookingRequest.Date
+            });
 
             return new RoomBookingResult
             {
